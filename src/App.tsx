595144c6b1c168ed8daab3be3d231b9dfa09f45b
Anastasiia Perova <perovaa.24@gmail.com>
2023-09-07 18:35:31 +0400

    add foldder components & start Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Home } from 'pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>main</>} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
