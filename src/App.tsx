f2708464eb746b9bccb00ac42ed63d686cb692fd
Anastasiia Perova <perovaa.24@gmail.com>
2023-09-07 17:25:59 +0400

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <div>......</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<>main</>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
