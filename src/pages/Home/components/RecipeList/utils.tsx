a794d367a6c0e8ba0fbf05d6e73d66cdd43f7b26
Anastasiia Perova <perovaa.24@gmail.com>
2023-09-12 14:51:06 +0400

    fix cast of util function

--- src/pages/Home/components/RecipeList/utils.tsx
+++ src/pages/Home/components/RecipeList/utils.tsx
@@ -6,25 +6,22 @@ import { IRecipeData } from 'configs/RecipeInterface';
 
 type recipeProps = Pick<IRecipeData, 'id' | 'title' | 'readyInMinutes' | 'image' | 'nutrition'>;
 
-export function mapper(data: Array<recipeProps>) {
-  return data.map(
-    (recipe) =>
-      ({
-        title: recipe.title,
-        image: recipe.image,
-        subtitle: recipe?.nutrition?.ingredients.map((ingredient) => ingredient.name).join(' + '),
-        captionSlot: recipe?.readyInMinutes ? recipe?.readyInMinutes + ' minutes' : 'unknown',
-        actionSlot: (
-          <Link to={`/recipes/${recipe.id}`} style={{ textDecoration: 'none' }}>
-            <Button>Details</Button>
-          </Link>
-        ),
-        contentSlot: (
-          <Text color="accent" maxLines={1}>
-            {recipe?.nutrition?.nutrients?.find((nutrient) => nutrient.name === 'Calories')?.amount + ' kcal'}
-          </Text>
-        ),
-        key: recipe.id,
-      }) as unknown as CardProps & { key: string },
-  );
+export function mapper(data: Array<recipeProps>): CardProps[] {
+  return data.map((recipe) => ({
+    title: recipe.title,
+    image: recipe.image,
+    subtitle: recipe?.nutrition?.ingredients.map((ingredient) => ingredient.name).join(' + '),
+    captionSlot: recipe?.readyInMinutes ? recipe?.readyInMinutes + ' minutes' : 'unknown',
+    actionSlot: (
+      <Link to={`/recipes/${recipe.id}`} style={{ textDecoration: 'none' }}>
+        <Button>Details</Button>
+      </Link>
+    ),
+    contentSlot: (
+      <Text color="accent" maxLines={1}>
+        {recipe?.nutrition?.nutrients?.find((nutrient) => nutrient.name === 'Calories')?.amount + ' kcal'}
+      </Text>
+    ),
+    key: recipe.id,
+  }));
 }
