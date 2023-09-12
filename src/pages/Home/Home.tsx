e5d546dddf34eacad7745fa86b81085e23590173
Anastasiia Perova <perovaa.24@gmail.com>
2023-09-12 15:14:26 +0400

    add type to page Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import { Header } from 'components/Header';
 import { Banner } from './components/Banner';
 import { RecipeList } from './components/RecipeList';
 
-export const Home = () => {
+export const Home: React.FC = () => {
   return (
     <>
       <Header />
