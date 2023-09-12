2754eab5895a30a7a0ecd1335c9a0cab87a34835
Anastasiia Perova <perovaa.24@gmail.com>
2023-09-12 15:12:08 +0400

    reorder imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import './styles/index.css';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
