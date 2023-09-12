99298c225f8aa78ca9bb62fb04f7055fd3a25432
Anastasiia Perova <perovaa.24@gmail.com>
2023-09-12 17:02:03 +0400

    add type for CardList component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import Card, { CardProps } from 'components/Card';
 
-export const CardList: React.FC<{ cards: Array<CardProps> }> = ({ cards }) => {
+type CardListProps = {
+  cards: CardProps[];
+};
+
+export const CardList: React.FC<CardListProps> = ({ cards }) => {
   return (
     <>
       {cards.map((card) => (
