b12ab517d245d103ba7859e38737c2faccdd2719
Anastasiia Perova <perovaa.24@gmail.com>
2023-09-13 20:01:08 +0400

    add placeholder 'No data' for RecipeCard

--- src/pages/Recipe/components/RecipeCard/RecipeCard.tsx
+++ src/pages/Recipe/components/RecipeCard/RecipeCard.tsx
@@ -55,99 +55,104 @@ export const RecipeCard: React.FC = () => {
   }
 
   return (
-    <div className={styles.wrapper}>
-      <section className={cn(styles['recipe-title'], styles['wrapper-item'])}>
-        <Link to={'..'} className={styles['recipe-title-link']}>
-          <ArrowDownIcon className={styles['recipe-title-link-icon']} color={ColorType.accent} />
-        </Link>
-        <Text view={TextViewType.title}>{data?.title}</Text>
-      </section>
-      <section className={cn(styles.section, styles['wrapper-item'])}>
-        <div className={styles['basic-info-wrapper']}>
-          <img src={data?.image} className={styles.image} />
-        </div>
-        <div className={styles['basic-info-wrapper']}>
-          <div className={styles['basic-info']}>
-            <div className={styles['basic-info-section']}>
-              <Text maxLines={1}>Preparation</Text>
-              <Text maxLines={1} color={ColorType.accent}>
-                {data?.preparationMinutes === -1 ? data?.readyInMinutes * 0.1 : data?.preparationMinutes} minutes
-              </Text>
+    <>
+      {data && !loading && (
+        <div className={styles.wrapper}>
+          <section className={cn(styles['recipe-title'], styles['wrapper-item'])}>
+            <Link to={'..'} className={styles['recipe-title-link']}>
+              <ArrowDownIcon className={styles['recipe-title-link-icon']} color={ColorType.accent} />
+            </Link>
+            <Text view={TextViewType.title}>{data.title}</Text>
+          </section>
+          <section className={cn(styles.section, styles['wrapper-item'])}>
+            <div className={styles['basic-info-wrapper']}>
+              <img src={data.image} className={styles.image} />
             </div>
-            <div className={styles['basic-info-section']}>
-              <Text maxLines={1}>Cooking</Text>
-              <Text maxLines={1} color={ColorType.accent}>
-                {data?.cookingMinutes === -1 ? data?.readyInMinutes * 0.9 : data?.cookingMinutes} minutes
-              </Text>
+            <div className={styles['basic-info-wrapper']}>
+              <div className={styles['basic-info']}>
+                <div className={styles['basic-info-section']}>
+                  <Text maxLines={1}>Preparation</Text>
+                  <Text maxLines={1} color={ColorType.accent}>
+                    {data.preparationMinutes === -1 ? data.readyInMinutes * 0.1 : data.preparationMinutes} minutes
+                  </Text>
+                </div>
+                <div className={styles['basic-info-section']}>
+                  <Text maxLines={1}>Cooking</Text>
+                  <Text maxLines={1} color={ColorType.accent}>
+                    {data.cookingMinutes === -1 ? data.readyInMinutes * 0.9 : data.cookingMinutes} minutes
+                  </Text>
+                </div>
+                <div className={styles['basic-info-section']}>
+                  <Text maxLines={1}>Total</Text>
+                  <Text maxLines={1} color={ColorType.accent}>
+                    {data.readyInMinutes} minutes
+                  </Text>
+                </div>
+                <div className={styles['basic-info-section']}>
+                  <Text maxLines={1}>Rating</Text>
+                  <Text maxLines={1} color={ColorType.accent}>
+                    {data.aggregateLikes} likes
+                  </Text>
+                </div>
+                <div className={styles['basic-info-section']}>
+                  <Text maxLines={1}>Serving</Text>
+                  <Text maxLines={1} color={ColorType.accent}>
+                    {data.servings} servings
+                  </Text>
+                </div>
+              </div>
             </div>
-            <div className={styles['basic-info-section']}>
-              <Text maxLines={1}>Total</Text>
-              <Text maxLines={1} color={ColorType.accent}>
-                {data?.readyInMinutes} minutes
-              </Text>
-            </div>
-            <div className={styles['basic-info-section']}>
-              <Text maxLines={1}>Rating</Text>
-              <Text maxLines={1} color={ColorType.accent}>
-                {data?.aggregateLikes} likes
-              </Text>
-            </div>
-            <div className={styles['basic-info-section']}>
-              <Text maxLines={1}>Serving</Text>
-              <Text maxLines={1} color={ColorType.accent}>
-                {data?.servings} servings
-              </Text>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className={styles['wrapper-item']}>
-        <div className={styles.description} dangerouslySetInnerHTML={{ __html: data?.summary ?? '' }}></div>
-      </section>
+          </section>
+          <section className={styles['wrapper-item']}>
+            <div className={styles.description} dangerouslySetInnerHTML={{ __html: data.summary ?? '' }}></div>
+          </section>
 
-      <section className={cn(styles['composition-section'], styles['wrapper-item'])}>
-        <div className={styles['composition-section-part']}>
-          <Text view={TextViewType.p20} className={styles['ingredients-title']}>
-            Ingredients
-          </Text>
-          <ul className={styles.ingredients}>
-            {data?.nutrition?.ingredients?.map((ingredient) => (
-              <li
-                key={ingredient?.id}
-                className={styles['ingredients-point']}
-              >{`${ingredient?.name} - ${ingredient?.amount} ${ingredient?.unit}`}</li>
-            ))}
-          </ul>
-        </div>
-        <div className={styles['composition-section-part']}>
-          <Text view={TextViewType.p20} className={styles['nutrients-title']}>
-            Nutrients
-          </Text>
-          <ul className={styles.nutrients}>
-            {data?.nutrition?.nutrients?.map((nutrient) => (
-              <li
-                key={nutrient?.name}
-                className={styles['nutrients-point']}
-              >{`${nutrient?.name} - ${nutrient?.amount} ${nutrient?.unit}`}</li>
-            ))}
-          </ul>
-        </div>
-      </section>
-      <section className={cn(styles['instruction-section'], styles['wrapper-item'])}>
-        <Text view={TextViewType.p20} className={styles['instructions-title']}>
-          Instructions
-        </Text>
-        <ul className={styles.instructions}>
-          {data?.analyzedInstructions?.[0].steps?.map((step) => (
-            <li key={step.number} className={styles['instructions-point']}>
-              <Text view={TextViewType.p16} color={ColorType.primary}>
-                Step {step.number}
+          <section className={cn(styles['composition-section'], styles['wrapper-item'])}>
+            <div className={styles['composition-section-part']}>
+              <Text view={TextViewType.p20} className={styles['ingredients-title']}>
+                Ingredients
               </Text>
-              <p>{step?.step}</p>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </div>
+              <ul className={styles.ingredients}>
+                {data.nutrition?.ingredients?.map((ingredient) => (
+                  <li
+                    key={ingredient?.id}
+                    className={styles['ingredients-point']}
+                  >{`${ingredient?.name} - ${ingredient?.amount} ${ingredient?.unit}`}</li>
+                ))}
+              </ul>
+            </div>
+            <div className={styles['composition-section-part']}>
+              <Text view={TextViewType.p20} className={styles['nutrients-title']}>
+                Nutrients
+              </Text>
+              <ul className={styles.nutrients}>
+                {data.nutrition?.nutrients?.map((nutrient) => (
+                  <li
+                    key={nutrient?.name}
+                    className={styles['nutrients-point']}
+                  >{`${nutrient?.name} - ${nutrient?.amount} ${nutrient?.unit}`}</li>
+                ))}
+              </ul>
+            </div>
+          </section>
+          <section className={cn(styles['instruction-section'], styles['wrapper-item'])}>
+            <Text view={TextViewType.p20} className={styles['instructions-title']}>
+              Instructions
+            </Text>
+            <ul className={styles.instructions}>
+              {data.analyzedInstructions?.[0].steps?.map((step) => (
+                <li key={step.number} className={styles['instructions-point']}>
+                  <Text view={TextViewType.p16} color={ColorType.primary}>
+                    Step {step.number}
+                  </Text>
+                  <p>{step?.step}</p>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      )}
+      {!data && !loading && <>NO data</>}
+    </>
   );
 };
