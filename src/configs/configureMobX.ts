61964fd3293cb69c8bb3605797a9f6276603f53d
Anastasiia Perova <perovaa.24@gmail.com>
2023-09-19 10:57:58 +0400

    move mobx config into main.tsx

--- src/configs/configureMobX.ts
+++ src/configs/configureMobX.ts
@@ -1,8 +1,11 @@
-import { configure } from 'mobx';
-
-configure({
+export const config = {
   useProxies: 'ifavailable',
   computedRequiresReaction: true,
   reactionRequiresObservable: true,
   observableRequiresReaction: true,
-});
+} as {
+  useProxies: 'ifavailable' | 'always' | 'never' | undefined;
+  computedRequiresReaction: boolean | undefined;
+  reactionRequiresObservable: boolean | undefined;
+  observableRequiresReaction: boolean | undefined;
+};
