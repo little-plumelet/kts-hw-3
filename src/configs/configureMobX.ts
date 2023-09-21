cea951ae9a5a9a7229bbdade3fdaa82ee8587938
Anastasiia Perova <perovaa.24@gmail.com>
2023-09-21 11:40:11 +0400

    create type for Mobx config

--- src/configs/configureMobX.ts
+++ src/configs/configureMobX.ts
@@ -1,11 +1,24 @@
-export const config = {
+type MpbixConfigType = {
+  enforceActions?: 'never' | 'always' | 'observed';
+  computedRequiresReaction?: boolean;
+  /**
+   * Warn if you try to create to derivation / reactive context without accessing any observable.
+   */
+  reactionRequiresObservable?: boolean;
+  /**
+   * Warn if observables are accessed outside a reactive context
+   */
+  observableRequiresReaction?: boolean;
+  isolateGlobalState?: boolean;
+  disableErrorBoundaries?: boolean;
+  safeDescriptors?: boolean;
+  reactionScheduler?: (f: () => void) => void;
+  useProxies?: 'always' | 'never' | 'ifavailable';
+};
+
+export const config: MpbixConfigType = {
   useProxies: 'ifavailable',
   computedRequiresReaction: true,
   reactionRequiresObservable: true,
   observableRequiresReaction: true,
-} as {
-  useProxies: 'ifavailable' | 'always' | 'never' | undefined;
-  computedRequiresReaction: boolean | undefined;
-  reactionRequiresObservable: boolean | undefined;
-  observableRequiresReaction: boolean | undefined;
 };
