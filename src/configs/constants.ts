952767a726179d2dd96141da1a7159890718ddcd
Anastasiia Perova <perovaa.24@gmail.com>
2023-10-03 16:45:55 +0400

    change RECIPES_PER_PAGE

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://api.spoonacular.com';
 // export const API_KEY = import.meta.env.VITE_SPINACULAR_API_KEY;
 export const API_KEY = process.env.VITE_SPINACULAR_API_KEY;
-export const RECIPES_PER_PAGE = 2;
+export const RECIPES_PER_PAGE = 9;
 export const SIMILAR_RECIPES_PER_PAGE = 3;
